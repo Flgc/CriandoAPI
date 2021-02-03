6ddd779e233892a9f68c5ede4e83ab0f8c198365
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-03 16:30:17 -0300

    Implementando rotas e parametros ((query params) nos metodos GET, PUT e POST

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title } = request.query;
+  console.log(title);
+
   return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
 });
 
 app.post('/projects', (request, response) => {
+  const bory = request.body;
+  console.log(bory);
+
   return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3', 'Projeto 4']);
 });
 
-app.put('/projects', (resquest, response) => {
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
   return response.json(['Projeto 5', 'Projeto 2', 'Projeto 3', 'Projeto 4']);
 });
 
