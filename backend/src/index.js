e8658604c14793526db53b62ded24d6d5c2be1fb
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-03 14:25:20 -0300

    Implementando metodos POST, PUSH e DELETE (estatico)

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-  return response.json({
-    message: 'Hello Dev!',
-  });
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3', 'Projeto 4']);
+});
+
+app.put('/projects', (resquest, response) => {
+  return response.json(['Projeto 5', 'Projeto 2', 'Projeto 3', 'Projeto 4']);
+});
+
+app.delete('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 4']);
 });
 
 app.listen(3333, () => {
