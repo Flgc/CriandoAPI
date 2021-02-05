4e80f6d6fcb2cccc79692bc4d304bcd5f5496459
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-04 22:30:35 -0300

    API Node.js com Typescript - Usando Token JWT para Autenticacao

--- backend-node-typescript/src/routes/index.ts
+++ backend-node-typescript/src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import itemsRoutes from './items_routes';
 import locationsRouter from './locations_routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/items', itemsRoutes);
 routes.use('/locations', locationsRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
