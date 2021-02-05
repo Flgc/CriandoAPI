4af63ff00fabbe1050283e31c2b791c8f331d04a
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-04 21:31:06 -0300

    API Node.js com Typescript - Upload de Arquivos com a Lib

--- backend-node-typescript/src/routes/locations_routes.ts
+++ backend-node-typescript/src/routes/locations_routes.ts
@@ -1,7 +1,11 @@
-import { Router, response } from 'express';
+import { request, Router, response } from 'express';
+import multer from 'multer';
 import knex from '../database/conection';
+import multerConfig from '../config/multer';
 
 const locationsRouter = Router();
+
+const upload = multer(multerConfig);
 
 locationsRouter.get('/', async (request, response) => {
   const { city, uf, items } = request.query;
@@ -89,5 +93,30 @@ locationsRouter.post('/', async (request, response) => {
     ...location,
   });
 });
+
+locationsRouter.put(
+  '/:id',
+  upload.single('image'),
+  async (request, response) => {
+    const { id } = request.params;
+
+    const image = request.file.filename;
+
+    const location = await knex('locations').where('id', id).first();
+
+    if (!location) {
+      return response.status(400).json({ message: 'Location not found.' });
+    }
+
+    const locationUpdated = {
+      ...location,
+      image,
+    };
+
+    await knex('locations').update(locationUpdated).where('id', id);
+
+    return response.json(locationUpdated);
+  }
+);
 
 export default locationsRouter;
