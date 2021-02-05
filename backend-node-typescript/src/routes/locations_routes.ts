8acff3cfeb1530b54f173d383473581e01ead239
Fabio Luis <ml@fabiotecnico1.biz>
2021-02-04 22:00:06 -0300

    API Node.js com Typescript - Validando Dados de uma

--- backend-node-typescript/src/routes/locations_routes.ts
+++ backend-node-typescript/src/routes/locations_routes.ts
@@ -1,5 +1,6 @@
 import { request, Router, response } from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 import knex from '../database/conection';
 import multerConfig from '../config/multer';
 
@@ -44,58 +45,78 @@ locationsRouter.get('/:id', async (request, response) => {
   return response.json({ location, items });
 });
 
-locationsRouter.post('/', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const location = {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  };
-
-  const transaction = await knex.transaction();
-
-  const newIds = await transaction('locations').insert(location);
-
-  const location_id = newIds[0];
-
-  const locationItens = items.map(async (item_id: number) => {
-    const selectedItem = await transaction('items')
-      .where('id', item_id)
-      .first();
-
-    if (!selectedItem) {
-      return response.status(400).json({ message: 'Item not Found.' });
+locationsRouter.post(
+  '/',
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
     }
+  ),
+  async (request, response) => {
+    const {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items,
+    } = request.body;
 
-    return {
-      item_id,
-      location_id,
+    const location = {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
     };
-  });
 
-  await transaction('location_items').insert(locationItens);
+    const transaction = await knex.transaction();
 
-  await transaction.commit();
+    const newIds = await transaction('locations').insert(location);
 
-  return response.json({
-    id: location_id,
-    ...location,
-  });
-});
+    const location_id = newIds[0];
+
+    const locationItens = items.map(async (item_id: number) => {
+      const selectedItem = await transaction('items')
+        .where('id', item_id)
+        .first();
+
+      if (!selectedItem) {
+        return response.status(400).json({ message: 'Item not Found.' });
+      }
+
+      return {
+        item_id,
+        location_id,
+      };
+    });
+
+    await transaction('location_items').insert(locationItens);
+
+    await transaction.commit();
+
+    return response.json({
+      id: location_id,
+      ...location,
+    });
+  }
+);
 
 locationsRouter.put(
   '/:id',
--- backend-node-typescript/src/server.ts
+++ backend-node-typescript/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 👷');
